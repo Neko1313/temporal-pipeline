4f69b482956726d0c37107254d7f510caabd7961
Neko1313 <89246763535@mail.ru>
2025-07-07 16:58:18 +1000

    fix

---
+++ core/tests/test_command.py
@@ -0,0 +1,8 @@
+import logging
+
+from tests.base import TemporalContainer
+
+
+def test_command(temporal_container_service: TemporalContainer) -> None:
+    logging.info(temporal_container_service.get_temporal_url())
+    assert 1
